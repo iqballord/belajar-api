3f667de0a2f5bcbc7e7f7f2e7f46d417f87a71fd
holla Iqbal <hollahollaiqbal@gmail.com>
2024-06-23 11:40:10 +0700

    update Project

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,29 +1,50 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
-export const protect = (req: Request, res: Response, next: NextFunction) => {
+export const comparePasswords = (password, hash) => {
+  return bcrypt.compare(password, hash);
+};
+
+export const hashPassword = (password) => {
+  return bcrypt.hash(password, 5);
+};
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    {
+      id: user.id,
+      username: user.username,
+    },
+    process.env.JWT_SECRET,
+  );
+  return token;
+};
+
+export const protect = (req, res, next) => {
   const bearer = req.headers.authorization;
+
   if (!bearer) {
-    res.status(401).json({ message: 'SORRY' });
+    res.status(401);
+    res.json({ message: 'not authorized' });
     return;
   }
 
   const [, token] = bearer.split(' ');
+
   if (!token) {
-    res.status(401).send('Not authorized');
+    res.status(401);
+    res.json({ message: 'not valid token' });
     return;
   }
 
-  const secret = process.env.JWT_SECRET;
-  if (!secret) {
-    throw new Error('JWT_SECRET is not defined');
-  }
-
   try {
-    const payload = jwt.verify(token, secret) as JwtPayload;
-    req.user = payload;
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
     next();
   } catch (e) {
-    res.status(401).send('Not authorized');
+    console.error(e);
+    res.status(401);
+    res.json({ message: 'not valid tokenAAA' });
+    return;
   }
 };
